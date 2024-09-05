2b043c90fa35ae28b96020b8f3090ae622a0ec5b
Scott Ponder <saponder@earthlink.net>
2024-09-04 22:31:11 -0700

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Scott Ponder on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
